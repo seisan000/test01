93d8b68b43f681a64ad6c0da72aede3368590fd7
seisan000 <48874093+seisan000@users.noreply.github.com>
2019-03-27 03:00:12 +0900

    Add files via upload

--- main.c
+++ main.c
@@ -5,6 +5,7 @@
 #define HAICHI_TYPE 12
 #define FALSE 0
 #define TRUE 1
+#define BLOCK_TYPE_MAX 4
 
 int mapping(int block[N_BLOCK], int orgin_map[Y_MAX][X_MAX]);
 int hantei(int map[Y_MAX][X_MAX] ,int y, int x, int type , int num );
@@ -12,12 +13,37 @@ int assign(int map[Y_MAX][X_MAX] ,int y, int x, int type , int num);
 int allset(int map[Y_MAX][X_MAX],int y,int x,int t,int origin_map[Y_MAX][X_MAX]);
 int hyouka(int map[Y_MAX][X_MAX]);
 int hantei2(int map[Y_MAX][X_MAX] ,int y, int x, int type , int num );
+int allset2(int map[Y_MAX][X_MAX],int y,int x,int origin_map[Y_MAX][X_MAX],int block[BLOCK_TYPE_MAX]);
 int G1;
+int score;
 int main (void){
 	
 	int origin_map[Y_MAX][X_MAX]=
 	{
-		
+		/*
+		{1,0,3,0,0,0,0,0,0,0},
+		{0,1,0,0,0,0,0,0,0,0},
+		{0,0,2,0,0,0,0,0,0,0},
+		{0,0,0,0,0,0,0,0,0,0},
+		{0,0,0,0,0,0,0,0,0,0},
+		{0,0,0,0,0,0,0,0,0,0},
+		{0,0,0,0,0,0,0,0,0,0},
+		{0,0,0,0,0,0,0,0,0,0},
+		{0,0,0,0,0,0,0,0,0,0},
+		{0,0,0,0,0,0,0,0,0,0}
+		*/
+		{1,0,3,0,0,0,0,0,0,0},
+		{0,1,0,0,0,0,0,0,0,0},
+		{0,0,2,0,0,0,0,0,0,0},
+		{0,0,0,0,0,0,0,0,0,0},
+		{0,0,0,0,0,0,0,0,0,0},
+		{0,0,0,0,0,0,0,0,0,0},
+		{0,0,0,0,0,0,0,0,0,0},
+		{0,0,0,0,0,0,0,0,0,0},
+		{0,0,0,0,0,0,0,0,0,0},
+		{0,0,0,0,0,0,0,0,0,0}
+
+		/*
 		{1,0,3,0,0,0,0,0,0,0},
 		{0,1,0,0,0,0,0,0,0,0},
 		{0,0,2,0,0,0,0,0,0,0},
@@ -26,8 +52,9 @@ int main (void){
 		{0,0,0,0,0,2,3,2,0,0},
 		{0,0,0,0,0,0,2,0,0,0},
 		{0,0,0,0,0,0,0,0,0,0},
-		{0,3,0,0,0,0,0,0,0,1},
+		{0,3,0,0,0,0,0,0,0,0},
 		{0,3,2,1,0,0,0,0,0,0}
+		*/
 		/*
 		{1,1,3,0,0,0,0,0,0,3},
 		{0,1,0,0,0,0,3,0,0,2},
@@ -41,11 +68,11 @@ int main (void){
 		{0,3,2,1,0,0,0,0,0,1}
 		*/
 	};
-	int origin_block[N_BLOCK]={1,1,1,1};
+	int origin_block[N_BLOCK]={2,2,2,1};
 	int i,j;
 	G1 = 0;
 	
-
+	score = 0;
 	for(i = 0; i < N_BLOCK; i++)
 	{
 		printf("%d ",origin_block[i]);
@@ -92,12 +119,13 @@ int mapping(int block[N_BLOCK], int origin_map[Y_MAX][X_MAX])
 	int x = 2;
 	int type;
 	int num = 2;
+	//int block[BLOCK_TYPE_MAX]={4,6,4,4};
 
 	for(y = 0; y < Y_MAX; y++)
 	{
 		for(x = 0; x < X_MAX; x++)
 		{
-			result_map[y][x] = origin_map[y][x];
+			//result_map[y][x] = origin_map[y][x];
 		}
 	}
 	/*
@@ -115,28 +143,28 @@ int mapping(int block[N_BLOCK], int origin_map[Y_MAX][X_MAX])
 	*/
 
 	//allset( origin_map,0,0,0,origin_map);
-	printf("hyoka = %d\n",hyouka(origin_map));
+	/*printf("hyoka = %d\n",hyouka(origin_map));
 	for(y = 0; y < 13; y++)
 	{
 		if(hantei2(origin_map ,9, 9, y, 1) == FALSE)
 		{
 			printf(" %d\n",y);
 		}
+	}*/
+	if( TRUE == allset2( result_map,0,0,origin_map,block))
+	{
+		printf("成功\n");
 	}
-
+	else
+	{
+		printf("失敗\n");
+	}
+	
 	for(y = 0; y < Y_MAX; y++)
 	{
 		for(x = 0; x < X_MAX; x++)
 		{
-			//origin_map[y][x] = result_map[y][x];
-		}
-	}
-
-	for(y = 0; y < Y_MAX; y++)
-	{
-		for(x = 0; x < X_MAX; x++)
-		{
-			printf("%d ", origin_map[y][x]);
+			printf("%d ", result_map[y][x]);
 		}
 		printf("\n");
 	}
@@ -735,73 +763,320 @@ int hyouka(int map[Y_MAX][X_MAX]){
 
 //非制約ブロック配置関数
 
-int allset2(int map[Y_MAX][X_MAX],int y,int x,int t,int origin_map[Y_MAX][X_MAX])
+int allset2(int sub_map[Y_MAX][X_MAX],int y,int x,int origin_map[Y_MAX][X_MAX],int block[BLOCK_TYPE_MAX])
 {
 	int i,j;
 	int posY,posX,type;
 	int bufx=0;
-
-	int sub_map[Y_MAX][X_MAX]={0};
+	int n_block;
+	int color;
+	int n_color;
+	int buf_block[BLOCK_TYPE_MAX];
+	//int block[BLOCK_TYPE_MAX];
+	//int sub_map[Y_MAX][X_MAX]={0};
+	int sub_map2[Y_MAX][X_MAX]={0};
+	int buf_map[Y_MAX][X_MAX]={0};
 	
-	//転写
-	for(i = 0; i < Y_MAX; i++)
-	{
-		for(j = 0; j < X_MAX; j++)
-		{
-			sub_map[i][j] = map[i][j];
-		}
-	}
-	G1++;
+	//G1++;
 	bufx = x;
 	for(posY = y; posY < Y_MAX; posY++)
 	{
-		for(posX = bufx; posX < X_MAX; posX++)
-		{	
-			for(type = t; type < HAICHI_TYPE+1; type++)
+		
+		for(posX = bufx; posX < X_MAX; posX+=2)
+		{
+			/*for(color = 0,n_color = 0; color < BLOCK_TYPE_MAX; color++)
 			{
-				if(hantei(sub_map,posY,posX,type,sub_map[posY][posX]))
-				{
+				n_color += block[color];
+			}
+			if( n_color == 0 )
+			{
+				return FALSE;
+			}*/
+
+			////ブロックを使い切った時終了条件を満たしているか確認
+			//for(color = 0,n_color = 0; color < BLOCK_TYPE_MAX; color++)
+			//{
+			//	n_color += buf_block[color];
+			//}
+			//if(n_color == 0 )
+			//{
+			//	printf("xx0\n");
+			//	if(TRUE == finishcheck(map, origin_map,buf_block))
+			//	{
+			//		printf("FINISH map\n");
+			//		for(i = 0; i < Y_MAX; i++)
+			//		{
+			//			for(j = 0; j < X_MAX; j++)
+			//			{
+			//				printf("%d ",sub_map[i][j]);
+			//			}
+			//			printf("\n");
+			//		}
+			//		printf("\n");
+			//		return TRUE;
+			//	}
+			//}
+
+			//配置指定があるときは必ず割当てる
+			if(origin_map[posY][posX] != sub_map[posY][posX] && origin_map[posY][posX] != 0 )
+			{
+				
+					//すでに配置されているときはスキップ
+
+					//ブロックがないときは中止
+					for(color = 0,n_color = 0; color < BLOCK_TYPE_MAX; color++)
+					{
+						n_color += block[color];
+					}
+					if( n_color <= 0 )
+					{
+						return FALSE;
+					}
+					if( block[origin_map[posY][posX]-1] <= 0 )
+					{
+						//printf("block[%d] = %d\n", origin_map[posY][posX]-1, block[origin_map[posY][posX]-1]);
+						return FALSE;
+					}
+
+				
+					//転写
+					for(i = 0; i < Y_MAX; i++)
+					{
+						for(j = 0; j < X_MAX; j++)
+						{
+							buf_map[i][j] = sub_map[i][j];
+						}
+					}
+
+					for(i = 0; i < BLOCK_TYPE_MAX; i++)
+					{
+						buf_block[i] = block[i];
+					}
+
+					for(type = 0; type < HAICHI_TYPE; type++)
+					{
+						if(hantei2(sub_map,posY,posX,type,origin_map[posY][posX]) )
+						{
 						
-					//割当て
-					assign(sub_map,posY,posX,type,sub_map[posY][posX]);
-					//printf("allset\n");
-					if(FALSE == allset(sub_map,posY,posX + 1,0,origin_map))
-					{
-						//失敗時マップ復元
-						for(i = 0; i < Y_MAX; i++)
-						{
-							for(j = 0; j < X_MAX; j++)
+							//printf("指定 before\n");
+							//for(i = 0; i < Y_MAX; i++)
+							//{
+							//	for(j = 0; j < X_MAX; j++)
+							//	{
+							//		printf("%d ",buf_map[i][j]);
+							//	}
+							//	printf("\n");
+							//}
+							//printf("posx=%d posy=%d type =%d color= %d\n",posX,posY,type,origin_map[posY][posX]);
+							//
+							//指定座標と現在配置を反映したマップ作製
+							for(i = 0; i < Y_MAX; i++)
 							{
-								sub_map[i][j] = map[i][j];
+								for(j = 0; j < X_MAX; j++)
+								{
+									//if(origin_map[i][j] != 0)
+									//{
+										buf_map[i][j] = sub_map[i][j];
+									//}
+								}
 							}
-						}
-					}
-					else
-					{
-						//成功時
-						for(i = 0; i < Y_MAX; i++)
-						{
-							for(j = 0; j < X_MAX; j++)
+							for(i = 0; i < BLOCK_TYPE_MAX; i++)
 							{
-									map[i][j] = sub_map[i][j];
+								buf_block[i] = block[i];
 							}
+							//割当て
+							assign(sub_map,posY,posX,type,origin_map[posY][posX]);
+
+							//printf("指定 after\n");
+							//for(i = 0; i < Y_MAX; i++)
+							//{
+							//	for(j = 0; j < X_MAX; j++)
+							//	{
+							//		printf("%d ",sub_map[i][j]);
+							//	}
+							//	printf("\n");
+							//}
+						
+
+							
+							block[origin_map[posY][posX]-1]--;
+							
+							//printf("指定 ");
+							//printf("%d使用\n\n",origin_map[posY][posX]);
+							//printf("allset\n");
+							if(FALSE == allset2(sub_map,posY,posX + 1,origin_map,block))
+							{
+								//失敗時マップ復元
+								for(i = 0; i < Y_MAX; i++)
+								{
+									for(j = 0; j < X_MAX; j++)
+									{
+										sub_map[i][j] = buf_map[i][j];
+									}
+								}
+								//block復元
+								for(i = 0; i < BLOCK_TYPE_MAX; i++)
+								{
+										block[i] = buf_block[i];
+								}
+								//printf("shitei false\n");
+								//return FALSE;
+							}
+							else
+							{
+								//break;
+								return TRUE;
+							}
+
 						}
-						break;
-						//return TRUE;
+					
 					}
+					return FALSE;
+				
+				
+			}
+			else//指定配置じゃないとき
+			{
+				
+				for(color = 0; color < BLOCK_TYPE_MAX; color++)
+				{
+					if(block[color] > 0 )
+					{
+						//printf("\n");
+
+						for(type = 0; type < HAICHI_TYPE+1; type++)
+						{
+							if(hantei2(sub_map,posY,posX,type,color+1))
+							{
+								//
+								//指定座標と現在配置を反映したマップ作製
+								for(i = 0; i < Y_MAX; i++)
+								{
+									for(j = 0; j < X_MAX; j++)
+									{
+										//if(origin_map[i][j] != 0)
+										//{
+											buf_map[i][j] = sub_map[i][j];
+										//}
+									}
+								}
+								for(i = 0; i < BLOCK_TYPE_MAX; i++)
+								{
+									buf_block[i] = block[i];
+								}
+								//printf(" before\n");
+								//for(i = 0; i < Y_MAX; i++)
+								//{
+								//	for(j = 0; j < X_MAX; j++)
+								//	{
+								//		printf("%d ",sub_map[i][j]);
+								//	}
+								//	printf("\n");
+								//}
+								//printf("posx=%d posy=%d type =%d color= %d\n",posX,posY,type,color+1);
+								//
+					
+								//割当て
+								assign(sub_map,posY,posX,type,color+1);
+
+
+								//printf(" after\n");
+								//for(i = 0; i < Y_MAX; i++)
+								//{
+								//	for(j = 0; j < X_MAX; j++)
+								//	{
+								//		printf("%d ",sub_map[i][j]);
+								//	}
+								//	printf("\n");
+								//}
+								//
+
+								if(!(type == HAICHI_TYPE))
+								{
+									block[color]--;
+								}
+								//printf("%d使用\n\n",color+1);
+								//printf("allset\n");
+								if(FALSE == allset2(sub_map,posY,posX + 1,origin_map,block))
+								{
+									//失敗時マップ復元
+									for(i = 0; i < Y_MAX; i++)
+									{
+										for(j = 0; j < X_MAX; j++)
+										{
+											sub_map[i][j] = buf_map[i][j];
+										}
+									}
+									//block復元
+									for(i = 0; i < BLOCK_TYPE_MAX; i++)
+									{
+										 block[i] = buf_block[i];
+									}
+									
+								}
+								else
+								{
+									//break;
+
+									return TRUE;
+								}
 						
 
 						
-					//break;
-				}
+								//break;
+							}
 					
+						}
+						/*
+						if(type == HAICHI_TYPE){
+							//allset(map,posY,posX,type);
+						
+							//失敗時マップ復元
+							for(i = 0; i < Y_MAX; i++)
+							{
+								for(j = 0; j < X_MAX; j++)
+								{
+									sub_map[i][j] = map[i][j];
+								}
+							}
+							//block復元
+							for(i = 0; i < BLOCK_TYPE_MAX; i++)
+							{
+									buf_block[i] = block[i];
+							}
+
+							return FALSE;
+						}
+						*/
+					}
+				}
 			}
 
-			if(type == HAICHI_TYPE){
-				//allset(map,posY,posX,type);
-				/*
-				printf("backtrack\n");
-				printf("posy=%d posx=%d \n",posY,posX);
+			//ブロックがないとき
+			n_block = 0;
+			for( i = 0; i < BLOCK_TYPE_MAX; i++)
+			{
+				n_block += block[i];
+			}
+			if( n_block <= 0)
+			{
+
+				//埋まっていない場所はないか？
+				for(i = 0; i < Y_MAX; i++)
+				{
+					for(j = 0; j < X_MAX; j++)
+					{
+						//1つでも埋まってない場所があったら終了
+						if((sub_map[i][j] & origin_map[i][j]) != origin_map[i][j] )
+						{
+							printf("BACK TRACK!!!!\n");
+							return FALSE;
+						}
+					}
+					//printf("\n");
+				}
+			
+				printf("COMPLEAT FINISH map!!\n");
 				for(i = 0; i < Y_MAX; i++)
 				{
 					for(j = 0; j < X_MAX; j++)
@@ -811,41 +1086,61 @@ int allset2(int map[Y_MAX][X_MAX],int y,int x,int t,int origin_map[Y_MAX][X_MAX]
 					printf("\n");
 				}
 				printf("\n");
+			
+				/*
+				printf("FINISH orign_map\n");
+				for(i = 0; i < Y_MAX; i++)
+				{
+					for(j = 0; j < X_MAX; j++)
+					{
+						printf("%d ",origin_map[i][j]);
+					}
+					printf("\n");
+				}
+				printf("\n");
+				printf("score = %d\n", hyouka(sub_map));
 				*/
 
-				return FALSE;
+				//成功時
+				
+				//break;
+				return TRUE;
+				//scoreチェック
+				if( score < hyouka(sub_map)){
+					score = hyouka(sub_map);
+				}
+				//map(0,0) != 0?
+
+				//孤立ブロックはないか？
 			}
-
-			
-
-		}//
-
-		//if(posY == Y_MAX -1 && posX == X_MAX -1 )
-		//{
-			//scoreチェック
-			
-			//map(0,0) != 0?
-
-			//孤立ブロックはないか？
-		//}
+	
+		}//posX
+		
 		
 		bufx = 0;
 	}
-	/*
+	n_block = 0;
+	for( i = 0; i < BLOCK_TYPE_MAX; i++)
+	{
+		printf("%d = %d\n",i+1,block[i]);
+		n_block += block[i];
+	}
+
+	
 	for(i = 0; i < Y_MAX; i++)
 	{
 		for(j = 0; j < X_MAX; j++)
 		{
-			printf("%d ",map[i][j]);
+			printf("%d ",sub_map[i][j]);
 		}
 		printf("\n");
 	}
 	printf("\n");
-	*/
+	
 	//変更後マップ反映
 	
-
-	return TRUE;
+	printf("xx11\n");
+	return FALSE;
 }
 
 //指定なしブロックを配置するか判定する関数
@@ -859,7 +1154,7 @@ int hantei2(int map[Y_MAX][X_MAX] ,int y, int x, int type , int num )
 
 	if(type == 12){
 		//置けるとか以前に実体がないから
-		printf("無を置こうとしているよ\n");
+		//printf("無を置こうとしているよ\n");
 		return TRUE;
 	}
 
@@ -1133,6 +1428,31 @@ int hantei2(int map[Y_MAX][X_MAX] ,int y, int x, int type , int num )
 			}
 		}
 
+		//孤立していても座標00ならOK
+		for(i = 0; i < 3; i++)
+		{
+			if( i == 0)
+			{
+				bufy = y;
+				bufx = x;
+			}
+			else if( i == 1)
+			{
+				bufy = block1y;
+				bufx = block1x;
+			}
+			else if( i == 2)
+			{
+				bufy = block2y;
+				bufx = block2x;
+			}
+			if(bufy == 0 && bufx == 0)
+			{
+				printf("0,0にいるのでセーフ\n");
+				return TRUE;
+			}
+			
+		}
 
 
 		//孤立ブロックではないか確認
@@ -1252,7 +1572,7 @@ int hantei2(int map[Y_MAX][X_MAX] ,int y, int x, int type , int num )
 			}
 			if(check == 0)
 			{
-				printf("孤立\n");
+				//printf("孤立a\n");
 				return FALSE;
 			}
 		}
@@ -1281,3 +1601,36 @@ int hantei2(int map[Y_MAX][X_MAX] ,int y, int x, int type , int num )
 }
 
 //残りブロック数で判定する関数を追加
+
+//完成しているかチェック関数
+int finishcheck(int map[Y_MAX][X_MAX],int origin_map[Y_MAX][X_MAX],int block[BLOCK_TYPE_MAX])
+{
+	int i,j;
+
+	for(i = 0; i < BLOCK_TYPE_MAX; i++)
+	{
+		if(block[i] != 0)
+		{
+			return FALSE;
+		}
+	}
+
+	for(i = 0; i < Y_MAX; i++)
+	{
+		for(j = 0; j < X_MAX; j++)
+		{
+			//1つでも埋まってない場所があったら終了
+			if((map[i][j] & origin_map[i][j]) != origin_map[i][j] )
+			{
+				//printf("y=%d x=%d map[y][x]= %d origin_map[i][j] = %d\n",i,j,map[i][j],origin_map[i][j]);
+				return FALSE;
+			}
+		}
+		//printf("\n");
+	}
+
+
+
+	return TRUE;
+
+}
